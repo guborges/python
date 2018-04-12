c4efc4443588f80fb8e54e400487a40b4c3ea04d
Gustavo Borges Evangelista <gustavoborges2@gmail.com>
2018-04-12 10:52:07 -0300

    exercicio

---
+++ exercicios-scripts-py/idade.py
@@ -0,0 +1,4 @@
+anoAtual = input("Digite o ano atual\n")
+anoNascimento = input("Digite o ano em que você nasceu\n")
+resultado = int(anoAtual) - int(anoNascimento)
+print("Você tem", resultado, "anos de idade")
